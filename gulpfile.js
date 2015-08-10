fdf5c657ff94d3a87dde8c8bf50c93cf570b2a6c
Yevgen Bushuyev <yevgen.bushuyev@gmail.com>
2015-08-10 22:48:09 +0200

    all in one works

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var fs = require('fs-extra');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var http = require('http');
-
+var Builder = require('systemjs-builder');
+var vulcanize = require('gulp-vulcanize');
+var minifyHTML = require('gulp-minify-html');
 
 var wctTest = require('web-component-tester').gulp.init(gulp);
 
@@ -24,10 +26,11 @@ gulp.task('clean', function () {
 gulp.task('copy-assets', ['clean'], function() {
   fs.mkdirSync('dist');
 
-
+  //TODO get rid of fs
   fs.copySync('bower_components', config.admin.dst+'bower_components');
   fs.copySync(config.admin.src, config.admin.dst);
-
+  fs.deleteSync(config.admin.dst+'elements/*.js');
+  fs.deleteSync(config.admin.dst+'elements/model/*.js');
 });
 
 
@@ -40,11 +43,10 @@ gulp.task('traceur', ['copy-assets'], function () {
       .pipe(gulp.dest(config.admin.dst+'elements/'));
 });
 
-gulp.task('serve', ['traceur'], function() {
+gulp.task('serve', ['systemjs'], function() {
   var app = express();
 
   app.use(function(req, res, next) {
-    console.log("headers");
     res.setHeader("Access-Control-Allow-Origin", "localhost");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -64,12 +66,44 @@ gulp.task('watch', ['serve'], function () {
     config.admin.src+'elements/**/*.html',
     config.admin.src+'elements/**/*.js',
     config.admin.src+'css/*.css'
-  ], ['traceur']);
+  ], ['systemjs']);
+});
+
+gulp.task('systemjs', ['copy-assets'], function(done){
+  return new Builder({
+    baseURL: '.',
+
+    // opt in to Babel for transpiling over Traceur
+    transpiler: 'traceur'
+
+    // etc. any SystemJS config
+  }).buildSFX('src/elements/all.js', 'dist/elements.js', { sfxGlobalName: 'elements', minify: false, sourceMaps:false })
+
+      .then(function() {
+        console.log('Build complete');
+
+  }).catch(function(err) {
+        console.log('Build error');
+        console.log(err);
+    });
+
+});
+
+gulp.task('vulcanize', ['systemjs'], function () {
+  return gulp.src(config.admin.dst+'/index.html')
+      .pipe(vulcanize({
+        inlineScripts: true,
+        inlineCss: true,
+        stripComments: true
+      }))
+      .pipe(minifyHTML({
+        conditionals: true,
+        spare:true
+      }))
+      .pipe(gulp.dest(config.admin.dst+'release'));
 });
 
 
-
-
-gulp.task('wct', ['traceur', 'test:local'], function(done) {
+gulp.task('wct', ['systemjs', 'test:local'], function(done) {
 
 });
--- src/elements/my-test2.js
+++ src/elements/my-test2.js
@@ -1,14 +1,14 @@
 import {User} from 'src/elements/model/user.js';
 
-export var MyTest = {
-    is: "my-test",
+export var MyTest2 = {
+    is: "my-test2",
 
     method: function () {
       console.log("method, user=" + this.val);
     },
 
     ready: function () {
-      this.user = new User('Billy');
+      this.user = new User('Dilly');
       console.log("ready: val=" + this.val + " e=" + new Error().stack);
     }
 };
